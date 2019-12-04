98987d475727f4490a7017a525a020db68b6d628
Daniel Pendragon <carlos.coutinho5@etec.sp.gov.br>
2019-12-04 12:26:14 -0300

    Enviando parametros na requisição

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -2,28 +2,35 @@ import React, { Component } from 'react'
 import { withGoogleMap, withScriptjs, GoogleMap, Marker } from 'react-google-maps'
 import { MarkerClusterer } from 'react-google-maps/lib/components/addons/MarkerClusterer';
 import api from '../../services/api'
-import { MenuLateral, Form } from './style'
-
+import { Container, MenuLateral, Form, Agrupamento } from './style'
+import io from 'socket.io-client'
 
 class Map extends Component {
-        state = {
-            places: []
-        }
+    state = {
+        places: [],
+    }
 
-    async componentDidMount() {
-        const response = await api.get('/')
+    async componentDidUpdate() {
+        this.registerToSocket()
+        const resp = JSON.stringify(this.props.propplacemarked)
+        const response = await api.get(`/?placemarked=${resp}`)
         await this.setState({ places: response.data })
     }
 
-    getMaker(maker) {
-        alert("Vocêclicou na " + maker.name)
+    registerToSocket(){
+        const socket = io('http://localhost:3001');
     }
+
+    getMaker(maker) {
+        alert("Você clicou na " + maker.name)
+    }
+
 
 
     render() {
         return (
             <GoogleMap
-                defaultZoom={5}
+                defaultZoom={10}
                 defaultCenter={{ lat: -23.52710718577365, lng: -46.69994882618741 }}
             >
                 <MarkerClusterer
@@ -50,36 +57,84 @@ class Map extends Component {
 const WrappedMap = withScriptjs(withGoogleMap(Map))
 
 export default class MeuMapa extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            stategrid: 0
+            stategrid: 0,
+            displayMarker: {}
         }
+
         this.selectGroup = this.selectGroup.bind(this)
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleSubmitChange = this.handleSubmitChange.bind(this)
     }
+
     selectGroup(type) {
-        this.setState({stategrid: type})
+        this.setState({ stategrid: type })
     }
+
+    handleInputChange = async e => {
+        let { value } = e.target
+
+        if (value == 1) {
+            await this.setState({
+                displayMarker: { lat: [-23.921150, -23.990524], lng: [-46.297715, -46.384536] }
+            }); // Santos
+        }
+        else if (value == 2) {
+            await this.setState({
+                displayMarker: { lat: [-24.031940, -24.081513], lng: [-46.399934, -46.605015] }
+            }); // Praia Grande
+        }
+    }
+
+    handleSubmitChange = e => {
+        e.preventDefault()
+    }
+
     render() {
         return (
-            <div style={{ height: '100vh', width: '100vw', display: 'flex' }}>
+            <Container style={{ height: '100vh', width: '100vw', display: 'flex' }}>
                 <MenuLateral>
                     <header>
                         <h1>Contele</h1>
                     </header>
                     <Form>
-                        <label htmlFor="">Selecione a região</label>
-                        <select name="" id="">
-                            <option value="">Baixada Santista</option>
-                            <option value="">ABCD Paulista</option>
-                            <option value=""></option>
+
+                        <label>Selecione a região</label>
+
+                        <select
+                            name="displayMarker"
+                            onChange={this.handleInputChange}>
+                            <option value="0">Selecione...</option>
+                            <option value="1">Santos</option>
+                            <option value="2">Praia Grande</option>
+                            <option value="3">São Vicente</option>
+                            <option value="4">Guarujá</option>
+                            <option value="5">Mongaguá</option>
+                            <option value="6">Itanhaém</option>
                         </select>
-                        <div className="tipoMapa">
-                            <label htmlFor=""> Tipo de agrupamento </label> <br />
-                            <input onClick={() => this.selectGroup(0)} name="opcaoTipoMapa" type="radio" /> Marker <br />
-                            <input onClick={() => this.selectGroup(100)} name="opcaoTipoMapa" type="radio" /> Cluster <br />
-                            <input onClick={this.selectGroup} name="opcaoTipoMapa" type="radio" /> Heatmap
-                        </div>
+
+                        <Agrupamento>
+                            <label> Tipo de agrupamento </label>
+                            <div>
+                                <input
+                                    onClick={() => this.selectGroup(0)}
+                                    name="opcaoTipoMapa"
+                                    type="radio"
+                                />
+                                <span>Marker</span>
+                            </div>
+                            <div>
+                                <input
+                                    onClick={() => this.selectGroup(100)}
+                                    name="opcaoTipoMapa"
+                                    type="radio"
+                                />
+                                <span>Cluster</span>
+                            </div>
+                        </Agrupamento>
+                        <button type="submit">AGRUPAR</button>
                     </Form>
                 </MenuLateral>
                 <WrappedMap
@@ -89,8 +144,9 @@ export default class MeuMapa extends Component {
                     mapElement={<div style={{ height: '100%', width: '70vw' }} />}
                     style={{ height: '100vh', width: '70vw' }}
                     propgrid={this.state.stategrid}
+                    propplacemarked={this.state.displayMarker}
                 />
-            </div>
+            </Container>
         )
     }
 }
--- src/components/Map/style.js
+++ src/components/Map/style.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components'
 
+const Container = styled.div`
+    display: flex;
+    height: 100vh; 
+    width: 100vw;
+`;
+
 const MenuLateral = styled.div`
     display: flex;
     flex-direction:column;
@@ -17,14 +23,45 @@ const MenuLateral = styled.div`
     }
 `;
 
-const Form = styled.div`
+const Form = styled.form.attrs(props => ({
+    autoComplete: 'off'
+}))`
     display: flex;
     flex-direction: column;
     padding: 45px;
+    height: 45vh;
     select{
+        background:#ecececf0;
+        height: 40px;
+        padding: 5px;
+        border: none;
         border-radius: 5px;
-        height: 45px;
         outline: none;
     }
+    label{
+        font-size: 18px;
+        margin-bottom: 5px;
+        font-weight: bold;
+        color: #696969;;
+    }
+    button{
+        margin-top: 20px;
+        background-color: #38449b;
+        outline: none;
+        border: none;
+        border-radius: 5px;
+        height: 40px;
+        color: #FFF;
+        font-weight: bold;
+        cursor: pointer;
+    }
 `;
-export { MenuLateral, Form }
+const Agrupamento = styled.div`
+    display: flex;
+    flex-direction:column;
+    margin-top: 15px;
+    input{
+        margin-right: 5px;
+    }
+`;
+export {Container, MenuLateral, Form, Agrupamento }
